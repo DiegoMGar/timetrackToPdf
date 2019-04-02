f5da7671240de8c10b5997f28b3178717e8c5a1f
Diego Maroto <diego.diemg@gmail.com>
2019-04-02 20:46:48 +0200

    Lógica de reemplazo de la plantilla, archivos temporales, backup e ignores.

--- src/main.py
+++ src/main.py
@@ -7,28 +7,50 @@ curPath = os.path.dirname(os.path.abspath(__file__))
 folderOut = curPath + "/../out"
 if not os.path.isdir(folderOut):
     os.mkdir(folderOut)
+folderData = curPath + "/../data"
+if not os.path.isdir(folderData):
+    os.mkdir(folderData)
+folderDataBackup = folderData + "/backup"
+if not os.path.isdir(folderDataBackup):
+    os.mkdir(folderDataBackup)
+
 templateIndex = curPath + "/../public/index.html"
 templateFila = curPath + "/../public/nodos/linea.html"
 templateTmp = curPath + "/../public/tmp.html"
 
-dataFile = curPath + "/../data/data.json"
+dataFile = folderData + "/input.json"
 ahora = datetime.datetime.now()
 nombreOut = ahora.strftime("%Y%m%d_%H%M%S")
 
-data = ""
 with open(dataFile, "r") as f:
-    data = f.read()
-data = json.loads(data)
+    dataTxt = f.read()
+    with open(folderDataBackup + "/input." + ahora.strftime("%Y%m%d_%H%M%S") + ".json", "w") as f2:
+        f2.write(dataTxt)
 
-linea = ""
+data = json.loads(dataTxt)
+
+if not os.path.isfile(dataFile):
+    with open(dataFile, "w") as f:
+        f.write(dataTxt)
+
 with open(templateFila, "r") as f:
     linea = f.read()
-body = ""
+
 with open(templateIndex, "r") as f:
     body = f.read()
 
-tmpLinea = linea.replace("{{dia}}", "quetecuen")
-tmpBody = body.replace("{{lineas}}", tmpLinea)
+tmpLineas = []
+for i in range(len(data["lineas"])):
+    tmpLinea = linea.replace("{{dia}}", data["lineas"][i]["dia"])
+    tmpLinea = tmpLinea.replace("{{tarea}}", data["lineas"][i]["tarea"])
+    tmpLinea = tmpLinea.replace("{{tiempo}}", data["lineas"][i]["tiempo"])
+    tmpLinea = tmpLinea.replace("{{timestamp}}", data["lineas"][i]["timestamp"])
+    tmpLinea = tmpLinea.replace("{{notas}}", data["lineas"][i]["notas"])
+    tmpLineas.append(tmpLinea)
+
+tmpBody = body.replace("{{lineas}}", "\n".join(tmpLineas))
+tmpBody = tmpBody.replace("{{subtitulo}}", data["subtitulo"])
+tmpBody = tmpBody.replace("{{tiempoTotal}}", data["tiempoTotal"])
 with open(templateTmp, "w") as f:
     f.write(tmpBody)
 pdfkit.from_file(templateTmp, f"{folderOut}/{nombreOut}.pdf")
